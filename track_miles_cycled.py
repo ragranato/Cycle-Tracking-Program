41919c226a25e48070c6d6b2f4dd787a373639ae
ragranato <robert.granato@gmail.com>
2023-03-19 20:44:04 -0400

    added persistent tracking

--- track_miles_cycled.py
+++ track_miles_cycled.py
@@ -1,11 +1,26 @@
 from decimal import Decimal
-from tracking import *
 
-print("Hello! Welcome to the cycling tracker!")
-new_miles = Decimal(input("How many miles did you cycle today? "))
 
-def add_miles(miles, tracking.total, tracking.ride_count):
-    tracking.total += miles
-    tracking.ride_count += 1
-    print(f"You cycled {miles} miles today, your total is {tracking.total}")
-    return tracking.total, tracking.ride_count
+print("Hello! Welcome to the Cycling Tracker!\n")
+new_miles = Decimal(input("How many miles did you cycle today? \n"))
+
+def add_miles(miles):
+    total = read_miles()
+    with open("tracking.txt", "w+") as track:
+        try:            
+            total += miles
+            track.write(f"\n{total}")
+            print(f"You cycled {miles} miles today, your total is {total}")
+        except:           
+     
+            total = miles
+            track.write(f"{total}")
+            print(f"You cycled {miles} miles today, your total is {total}")
+    return total
+
+def read_miles():
+    with open("tracking.txt", "r") as file:
+        total = Decimal(file.read())
+    return total
+
+add_miles(new_miles)
--- tracking.py
+++
@@ -1,3 +0,0 @@
-total = 0
-avg_miles = 0
-ride_count = 0
